e4b655f7c57af799b69fb904854db67c9dedf82a
Zimon Kuhs <zmk.zk.dev@gmail.com>
2017-11-23 23:54:32 +0100

    Fixed serverless deploy not working -- now not including the entire AWS SDK in JAR.

--- membrum/Payment/src/main/java/com/zetta/payment/lambda/OrderLambda.java
+++ membrum/Payment/src/main/java/com/zetta/payment/lambda/OrderLambda.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
@@ -24,115 +25,113 @@ import com.zetta.payment.util.URLUtil;
  */
 public class OrderLambda extends Lambda {
 
-    private static Logger log = Logger.getLogger(OrderLambda.class);
+	private static Logger log = Logger.getLogger(OrderLambda.class);
 
-    private static final DynamoOrderDAO orderDAO = DynamoOrderDAO.instance();
+	private static final DynamoOrderDAO orderDAO = DynamoOrderDAO.instance();
 
-    public void dibsConfirmation(InputStream is, OutputStream os,
-            Context context) {
+	public void dibsConfirmation(InputStream is, OutputStream os, Context context) {
 
-        log.info("DIBS executed callback.");
+		log.info("DIBS executed callback.");
 
-        Map<String, String> headers = new LinkedHashMap<String, String>();
-        headers.put("content-type", "*/*");
+		Map<String, String> headers = new LinkedHashMap<String, String>();
+		headers.put("content-type", "*/*");
 
-        Response response = null;
-        try {
-            Order order = createNewOrder(is);
-            response = new Response(200, headers, "Transaction completed.");
-            order.setIsPaid(true);
-            saveOrder(order);
-        } catch (InvalidInput | ValidationFailed e) {
-            error(e.getMessage());
-            response = new Response(500, headers, errorJSON(log));
-        }
+		Response response = null;
+		try {
+			Order order = createNewOrder(is);
+			response = new Response(200, headers, "Transaction completed.");
+			order.setIsPaid(true);
+			saveOrder(order);
+		} catch (InvalidInput | ValidationFailed e) {
+			error(e.getMessage());
+			response = new Response(500, headers, errorJSON(log));
+		}
 
-        log.info("Sending response:\n    " + response.toString());
-        try {
-            os.write(response.asJson().getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		log.info("Sending response:\n    " + response.toString());
+		try {
+			os.write(response.asJson().getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private Order createNewOrder(InputStream inStream)
-            throws ValidationFailed, InvalidInput {
+	private Order createNewOrder(InputStream inStream) throws ValidationFailed, InvalidInput {
 
-        Map<String, String> parameters = URLUtil.decode(getBody(inStream));
-        String status = parameters.get("statuscode");
-        String orderId = parameters.get("orderid");
-        Optional<Order> maybeOrder = lambdaGetOrder(orderId);
+		Map<String, String> parameters = URLUtil.decode(getBody(inStream));
+		String status = parameters.get("statuscode");
+		String orderId = parameters.get("orderid");
+		Optional<Order> maybeOrder = lambdaGetOrder(orderId);
 
-        if (status == null) {
-            error("Erroneous callback format, no 'statuscode' parameter.");
-        } else if (!status.equals("2")) {
-            error("Transaction not completed, status code: " + status + ".");
-        }
+		if (status == null) {
+			error("Erroneous callback format, no 'statuscode' parameter.");
+		} else if (!status.equals("2")) {
+			error("Transaction not completed, status code: " + status + ".");
+		}
 
-        if (!maybeOrder.isPresent()) {
-            error("No order with the specified ID (" + orderId + ") exists:");
-        }
+		if (!maybeOrder.isPresent()) {
+			error("No order with the specified ID (" + orderId + ") exists:");
+		}
 
-        if (!hasErrors()) {
-            return maybeOrder.get();
-        }
+		if (!hasErrors()) {
+			return maybeOrder.get();
+		}
 
-        throw new ValidationFailed(errorString());
-    }
+		throw new ValidationFailed(errorString());
+	}
 
-    private String getBody(InputStream is) throws InvalidInput {
-        try {
-            Map<?, ?> json = JSONUtil.parse(is);
-            log.info("Received json with parameters:\n"
-                    + JSONUtil.prettyPrint(json));
+	private String getBody(InputStream is) throws InvalidInput {
+		try {
+			Map<?, ?> json = JSONUtil.parse(is);
+			log.info("Received json with parameters:\n" + JSONUtil.prettyPrint(json));
 
-            if (!json.containsKey("body")) {
-                throw new InvalidInput("No \"body\" key in object.");
-            }
-            return json.get("body").toString();
-        } catch (IOException e) {
-            throw new InvalidInput("Error parsing JSON object:\n"
-                    + e.getMessage().split("\n at")[0] + ".");
-        }
-    }
+			if (!json.containsKey("body")) {
+				throw new InvalidInput("No \"body\" key in object.");
+			}
+			return json.get("body").toString();
+		} catch (IOException e) {
+			throw new InvalidInput("Error parsing JSON object:\n" + e.getMessage().split("\n at")[0] + ".");
+		}
+	}
 
-    public Optional<Order> lambdaGetOrder(String orderId) {
-        Map<String, String> input = new LinkedHashMap<String, String>();
-        input.put("orderId", orderId);
+	private Optional<Order> lambdaGetOrder(String orderId) {
+		Map<String, String> input = new LinkedHashMap<String, String>();
+		input.put("orderId", orderId);
 
-        try {
-            Map<?, ?> result = callLambda("payment-prod-getOrder",
-                    JSONUtil.prettyPrint(input));
+		try {
+			Map<?, ?> result = callLambda("payment-prod-getOrder", JSONUtil.prettyPrint(input));
 
-            return Optional.of(JSONUtil.parse(result, Order.class));
-        } catch (IOException e) {
-            error(e.getMessage());
-        }
-        return Optional.empty();
-    }
+			return Optional.of(JSONUtil.parse(result, Order.class));
+		} catch (IOException e) {
+			error(e.getMessage());
+		}
+		return Optional.empty();
+	}
 
-    public Optional<Order> getOrder(String id) {
+	public void getOrder(InputStream is, OutputStream os, Context context) {
+		log.info("Received " + new Scanner(is).useDelimiter("\\A").next());
+		String id = "test";
 
-        log.info("Querying table for order " + id + ".");
-        Optional<Order> order = orderDAO.get(id);
-        if (order.isPresent()) {
-            log.info("Order existed.");
-        } else {
-            log.info("Order could not be found.");
-        }
-        return order;
-    }
+		log.info("Querying table for order " + id + ".");
+		Optional<Order> order = orderDAO.get(id);
+		if (order.isPresent()) {
+			log.info("Order existed.");
+		} else {
+			log.info("Order could not be found.");
+		}
 
-    public void saveOrder(Order order) {
+		log.info(order);
+	}
 
-        if (order == null) {
-            log.error("Can not save null order.");
-            throw new IllegalArgumentException("Can not save null order.");
-        }
+	public void saveOrder(Order order) {
 
-        log.info("Saving or updating order " + order.getOrderId() + ".");
-        orderDAO.save(order);
-        log.info("Successfully saved order.");
-    }
+		if (order == null) {
+			log.error("Can not save null order.");
+			throw new IllegalArgumentException("Can not save null order.");
+		}
+
+		log.info("Saving or updating order " + order.getOrderId() + ".");
+		orderDAO.save(order);
+		log.info("Successfully saved order.");
+	}
 
 }
--- membrum/Payment/src/main/java/com/zetta/payment/pojo/Order.java
+++ membrum/Payment/src/main/java/com/zetta/payment/pojo/Order.java
@@ -9,72 +9,72 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 @DynamoDBTable(tableName = "MembrumOrders")
 public class Order implements Serializable {
 
-    private static final long serialVersionUID = 5685764812092864593L;
+	private static final long serialVersionUID = 5685764812092864593L;
 
-    public static final String ID_INDEX = "orderId";
-    public static final String AMOUNT_INDEX = "amount";
-    public static final String IS_PAID_INDEX = "isPaid";
-    public static final String USER_INDEX = "userId";
+	public static final String ID_INDEX = "orderId";
+	public static final String AMOUNT_INDEX = "amount";
+	public static final String IS_PAID_INDEX = "isPaid";
+	public static final String USER_INDEX = "userId";
 
-    private String orderId;
-    private String userId;
-    private int amount;
-    private boolean isPaid;
+	private String orderId;
+	private String userId;
+	private int amount;
+	private boolean isPaid;
 
-    public Order() {
-        this("", "", 0, false);
-    }
+	public Order() {
+		this("", "", 0, false);
+	}
 
-    public Order(String orderId, String userId, int amount, boolean isPaid) {
-        this.orderId = orderId;
-        this.userId = userId;
-        this.amount = amount;
-        this.isPaid = isPaid;
-    }
+	public Order(String orderId, String userId, int amount, boolean isPaid) {
+		this.orderId = orderId;
+		this.userId = userId;
+		this.amount = amount;
+		this.isPaid = isPaid;
+	}
 
-    @DynamoDBHashKey(attributeName = ID_INDEX)
-    public String getOrderId() {
-        return orderId;
-    }
+	@DynamoDBHashKey(attributeName = ID_INDEX)
+	public String getOrderId() {
+		return orderId;
+	}
 
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
 
-    @DynamoDBHashKey(attributeName = USER_INDEX)
-    public String getUserId() {
-        return userId;
-    }
+	@DynamoDBHashKey(attributeName = USER_INDEX)
+	public String getUserId() {
+		return userId;
+	}
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 
-    @DynamoDBAttribute(attributeName = AMOUNT_INDEX)
-    public int getAmount() {
-        return amount;
-    }
+	@DynamoDBAttribute(attributeName = AMOUNT_INDEX)
+	public int getAmount() {
+		return amount;
+	}
 
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
 
-    @DynamoDBAttribute(attributeName = IS_PAID_INDEX)
-    public boolean getIsPaid() {
-        return isPaid;
-    }
+	@DynamoDBAttribute(attributeName = IS_PAID_INDEX)
+	public boolean getIsPaid() {
+		return isPaid;
+	}
 
-    public void setIsPaid(boolean isPaid) {
-        this.isPaid = isPaid;
-    }
+	public void setIsPaid(boolean isPaid) {
+		this.isPaid = isPaid;
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder string = new StringBuilder();
-        string.append("Order id: " + orderId);
-        string.append("\nUser id: " + userId);
-        string.append("\nAmount: " + amount);
-        string.append("\nIs paid: " + (isPaid ? "yes" : "no") + ".");
-        return string.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder string = new StringBuilder();
+		string.append("Order id: " + orderId);
+		string.append("\nUser id: " + userId);
+		string.append("\nAmount: " + amount);
+		string.append("\nIs paid: " + (isPaid ? "yes" : "no") + ".");
+		return string.toString();
+	}
 }
