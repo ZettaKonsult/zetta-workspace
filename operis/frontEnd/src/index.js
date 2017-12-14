64a45b461d9567b83f5e6b2e040e93c2d9920c1f
Fredrik Palmquist <fiddep@telia.com>
2017-12-14 14:28:49 +0100

    Added semantic-ui to the project. Updated package.json scripts to take advantage of native functionallity

--- operis/frontEnd/src/index.js
+++ operis/frontEnd/src/index.js
@@ -5,6 +5,8 @@ import registerServiceWorker from './registerServiceWorker'
 import configureStore from './configureStore'
 import Root from './Root'
 
+import 'semantic-ui-css/semantic.min.css'
+
 const store = configureStore()
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
