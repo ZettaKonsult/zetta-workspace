21ec7fce0fbc1ceaeb96f5acafadc6366169e09f
Fredrik Palmquist <fiddep@telia.com>
2017-12-22 17:35:45 +0100

    Styled worker page

--- operis/frontEnd/src/Container/Worker/Worker.js
+++ operis/frontEnd/src/Container/Worker/Worker.js
@@ -11,31 +11,60 @@ import {getVisibleWorkers} from '../../reducers'
 
 class Worker extends Component {
   callback = () => {
-    this.props.history.push('/')
+    this.props.history.push('/worker')
   }
 
+  newWorker = () => {
+    this.props.history.push('/worker/0')
+  }
+
+  renderList = props =>
+    props.workers.map(worker => (
+      <WorkerCard
+        key={worker.id}
+        name={worker.name}
+        onClick={() => this.props.deleteWorker(worker.id)}
+      />
+    ))
+
   render() {
+    const {id} = this.props.match.params
     return (
       <div>
-        <WorkerForm callback={this.callback} id={this.props.match.params.id} />
-        <Button as={Link} to="/">
-          Cancel
-        </Button>
-        {this.props.workers.map(worker => (
-          <WorkerCard
-            key={worker.id}
-            name={worker.name}
-            onClick={() => this.props.deleteWorker(worker.id)}
-          />
-        ))}
+        {!!id ? (
+          <div>
+            <WorkerForm callback={this.callback} id={id} />
+            <Button onClick={this.callback} content="Cancel" />
+          </div>
+        ) : (
+          <div>
+            <Button
+              fluid
+              primary
+              onClick={this.newWorker}
+              content="New Worker"
+            />
+            {this.renderList(this.props)}
+          </div>
+        )}
       </div>
     )
   }
 }
 
 const WorkerCard = props => (
-  <div>
-    {props.name}
+  <div
+    style={{
+      padding: '0.5em',
+      border: '1px solid grey',
+      margin: '0.2em',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      borderRadius: '5px'
+    }}
+  >
+    <bold style={{fontSize: '1.2em'}}>{props.name}</bold>
     <Icon onClick={props.onClick} link name="close" />
   </div>
 )
