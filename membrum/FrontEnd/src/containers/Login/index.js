578bec7ae9c41b9fb3bbf2a38d4b2925fdfde17c
Fredrik Palmquist <fiddep@telia.com>
2018-01-04 18:40:23 +0100

    deleted unnessecary file

--- membrum/FrontEnd/src/containers/Login/index.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import LoginForm from './LoginForm'
-import {loginUser} from '../../user/authenticationActions'
-
-export default connect(undefined, {
-  onSubmit: values => loginUser(values.username, values.password)
-})(LoginForm)
