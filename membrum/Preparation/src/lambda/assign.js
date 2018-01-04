b599018a9d5d6f244325eb04d8d16b914ce04c07
Zimon Kuhs <zmk.zk.dev@gmail.com>
2018-01-04 18:16:35 +0100

    Implemented union assignment.
    The Preparation module can now separate union assignments depending on
    whether a user should get a new, pick one, keep the same, or get a
    new one.

--- membrum/Preparation/src/lambda/assign.js
+++ membrum/Preparation/src/lambda/assign.js
@@ -5,9 +5,12 @@
  */
 
 import type { AWSCallback, AWSContext, AWSEvent } from './types'
+import { getOrganisationIds } from './util'
 import * as unionAssigner from '../unionAssigner'
-import { config } from '../config'
 import AWS from 'aws-sdk'
+import { config } from '../config'
+
+import util from 'util'
 
 AWS.config.update({ region: 'eu-central-1' })
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
@@ -17,16 +20,39 @@ export const getParseResults = async (
   context: AWSContext,
   callback: AWSCallback
 ) => {
-  const params = {
-    TableName: 'LadokParseResult'
+  let parseResult = {}
+  try {
+    parseResult = await dynamoDB
+      .scan({
+        TableName: 'LadokParseResult'
+      })
+      .promise()
+    parseResult = parseResult.Items
+  } catch (error) {
+    callback(error)
   }
 
-  dynamoDB.scan(params, (error, data) => {
-    if (error) {
-      callback(error)
-    }
-    const faculties = unionAssigner.getFaculties(data.Items)
-    const trfMap = config.TRF.UnionMapping
-    callback(null, unionAssigner.getUnions(trfMap, faculties))
-  })
+  const faculties = unionAssigner.getFaculties(parseResult)
+  const trfMap = await getOrganisationIds(config.TRF.UnionMapping)
+  const newAssignments = unionAssigner.getUnions(trfMap, faculties)
+
+  let userResult = {}
+  try {
+    userResult = await dynamoDB
+      .scan({
+        TableName: 'MembrumUsers'
+      })
+      .promise()
+    userResult = userResult.Items
+  } catch (error) {
+    callback(error)
+  }
+
+  callback(
+    null,
+    unionAssigner.getUpdatedUnions({
+      NewAssignments: newAssignments,
+      Users: userResult
+    })
+  )
 }
