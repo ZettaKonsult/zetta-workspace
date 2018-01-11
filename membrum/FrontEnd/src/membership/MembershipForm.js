fa121735802df8501431ffeb137b968897895f7d
Fredrik Palmquist <fiddep@telia.com>
2018-01-11 19:49:51 +0100

    Updated mock file. started using validation for membershipform

--- membrum/FrontEnd/src/membership/MembershipForm.js
+++ membrum/FrontEnd/src/membership/MembershipForm.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux'
 import { Field, FieldArray, reduxForm } from 'redux-form'
 
 import { membershipSave } from './membershipActions'
+import { getPlanDetails } from './membershipReducer'
+
+import planTemplate from './PlanTemplate'
+import db from '../mocks/db.json'
 
 const PlanOptions = ({ plans }) =>
   plans.map(item => (
@@ -11,8 +15,13 @@ const PlanOptions = ({ plans }) =>
     </option>
   ))
 
-const renderPlans = ({ fields, ...props }) => (
+const renderPlans = ({
+  fields,
+  meta: { touched, error, submitFailed },
+  ...props
+}) => (
   <div className="membershipGroup">
+    {(touched || submitFailed) && error && <span>{error}</span>}
     <div className="ButtonGroup">
       <button type="button" onClick={() => fields.push('17')}>
         Add Plan
@@ -26,8 +35,7 @@ const renderPlans = ({ fields, ...props }) => (
         <button
           type="button"
           title="Remove"
-          onClick={() => fields.remove(index)}
-        >
+          onClick={() => fields.remove(index)}>
           Remove Plan
         </button>
       </div>
@@ -37,7 +45,6 @@ const renderPlans = ({ fields, ...props }) => (
 
 let MembershipForm = props => {
   const { handleSubmit, submitting, pristine } = props
-
   return (
     <form onSubmit={handleSubmit} className="membership">
       <FieldArray
@@ -52,11 +59,41 @@ let MembershipForm = props => {
   )
 }
 
-MembershipForm = reduxForm({ form: 'MembershipForm' })(MembershipForm)
+const validate = (values, { getPlanDetails }) => {
+  const rules = db.plantemplates
+  const validator = planTemplate(rules)
+
+  const errors = {}
+  const mapPlans = values.plans.map(plan => getPlanDetails(plan))
+  const valid = validator.evaluatePlan(mapPlans)
+
+  if (!valid) {
+    errors.plans = { _error: 'AF is not a part of this' }
+    const error = validator.getErrors(mapPlans)
+    console.log(error)
+  } else {
+    const membersArrayErrors = []
+    values.plans.forEach((plan, planIndex) => {
+      const planErrors = {}
+      console.log(plan)
+      if (!plan || !plan.firstName) {
+        planErrors.firstName = 'Required'
+        membersArrayErrors[planIndex] = planErrors
+      }
+    })
+    if (membersArrayErrors.length) {
+      errors.members = membersArrayErrors
+    }
+  }
+  return errors
+}
+
+MembershipForm = reduxForm({ form: 'MembershipForm', validate })(MembershipForm)
 
 const mapStateToProps = (state, props) => ({
   allPlans: state.membershipReducer.allPlans,
-  initialValues: { plans: state.membershipReducer.plans }
+  initialValues: { plans: state.membershipReducer.plans },
+  getPlanDetails: id => getPlanDetails(state.membershipReducer)(id)
 })
 
 const mapDispatchToProps = {
