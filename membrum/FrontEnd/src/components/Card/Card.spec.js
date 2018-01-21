bd75a2eb3d024b4c122f65a4dc4cfadf426cb18d
Fredrik Palmquist <fiddep@telia.com>
2018-01-21 21:22:04 +0100

    Added react-test-renderer for snapshot testing, updated test to use it

--- membrum/FrontEnd/src/components/Card/Card.spec.js
+++ membrum/FrontEnd/src/components/Card/Card.spec.js
@@ -1,56 +1,13 @@
-import React from "react"
-import { shallow, mount } from "enzyme"
-import toJson from "enzyme-to-json"
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { StaticRouter } from 'react-router'
 
-import Card from "./"
-import Footer from "./Footer"
-import Text from "./Text"
+import Card from './'
+import Footer from './Footer'
 
-describe("<Card />", function() {
-  it("should render a Card", function() {
-    const wrapper = shallow(<Card />)
-    expect(toJson(wrapper)).toMatchSnapshot()
-  })
-
-  it("should render Text and Footer", function() {
-    const wrapper = shallow(<Card />)
-    expect(wrapper.containsAllMatchingElements([<Text />, <Footer />])).toEqual(
-      true
-    )
-  })
-
-  describe("Default values", function() {
-    it("Should have a text", function() {
-      const wrapper = mount(<Card />)
-      expect(wrapper.prop("text")).toEqual("New Comments")
-    })
-
-    it("Should have a value", function() {
-      const wrapper = mount(<Card />)
-      expect(wrapper.prop("value")).toEqual("24")
-    })
-
-    it("Should have a icon", function() {
-      const wrapper = mount(<Card />)
-      expect(wrapper.prop("icon")).toEqual("comments")
-    })
-    it("Should have a type", function() {
-      const wrapper = mount(<Card />)
-      expect(wrapper.prop("type")).toEqual("primary")
-    })
-  })
-})
-
-describe("<Footer />", function() {
-  it("Should render a Footer", function() {
-    const wrapper = shallow(<Footer />)
-    expect(toJson(wrapper)).toMatchSnapshot()
-  })
-})
-
-describe("<Text />", function() {
-  it("Should render a Text", function() {
-    const wrapper = shallow(<Text />)
-    expect(toJson(wrapper)).toMatchSnapshot()
+describe('<Card />', function() {
+  it('should render a Card', function() {
+    const tree = renderer.create(<Card />).toJSON()
+    expect(tree).toMatchSnapshot()
   })
 })
