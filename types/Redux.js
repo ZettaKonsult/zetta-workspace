405ecc5b006fdcfcf178033775dc7eec803c8b4d
Fredrik Palmquist <fiddep@telia.com>
2018-01-31 14:50:03 +0100

    Added redux types

---
+++ types/Redux.js
@@ -0,0 +1,11 @@
+export type Action = { +type: string, +payload?: Object };
+
+export type GetState = () => State;
+
+export type PromiseAction = Promise<Action>;
+
+export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
+
+export type Dispatch = (
+  action: Action | ThunkAction | PromiseAction | Array<Action>
+) => any;
