e077aad20f41d34f434a9602d260b18bd792324c
Zimon Kuhs <zmk.zk.dev@gmail.com>
2018-02-05 14:25:18 +0100

    Changed login to use zk-aws-users.

--- membrum/FrontEnd/src/config.js
+++ membrum/FrontEnd/src/config.js
@@ -1,4 +1,9 @@
 export default {
+  AWS_ACCESS_KEY_ID: 'AKIAI76EJH7VQ3MAG6PA',
+  ACCOUNT_ID: '460056602695',
+  AWS_UNAUTH_ROLE_ARN: 'arn:aws:iam::460056602695:role/Cognito_membrumidentitypoolUnauth_Role',
+  AWS_AUTH_ROLE_ARN: 'arn:aws:iam::460056602695:role/Cognito_membrumidentitypoolAuth_Role',
+  IDENTITY_POOL_ID: 'eu-central-1:6b37b86f-45d5-4b65-ac50-7f43750ea678',
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     BUCKET: 'ladok-files',
