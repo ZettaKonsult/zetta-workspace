aa5a733776cf8b94b62d7cbcbc83da45b61648d7
Fredrik Palmquist <fiddep@telia.com>
2018-02-09 16:35:09 +0100

    Added State type to Redux types

--- types/Redux.js
+++ types/Redux.js
@@ -11,3 +11,5 @@ export type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
 export type Dispatch = (
   action: Action | ThunkAction | PromiseAction | Array<Action>
 ) => any;
+
+export type State = Object
