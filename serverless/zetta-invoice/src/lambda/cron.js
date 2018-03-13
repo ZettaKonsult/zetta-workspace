5a3562e349a672132808afc9ecaa722626d54643
Fredrik Palmquist <fiddep@telia.com>
2018-03-13 09:22:25 +0100

    chore(cron dev): add cron development tools

---
+++ serverless/zetta-invoice/src/lambda/cron.js
@@ -0,0 +1,16 @@
+/* @flow */
+
+/**
+ * @date 2018-03-13
+ */
+
+import type { AWSEvent, AWSContext, AWSCallback } from 'types/AWS';
+
+import { success, failure } from '../util/response';
+import parser from '../util/parser';
+import db from '../util/database';
+
+export const main = (event, context) => {
+  const time = new Date();
+  console.log(`Your cron function "${context.functionName}" ran at ${time}`);
+};
