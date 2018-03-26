e93d9e1b04dd200791629363d56f562730a8703b
Fredrik Palmquist <fiddep@telia.com>
2018-03-26 15:07:09 +0200

    chore(build): Update package and scripts
    Update webpack and serverless-webpack.
    Remove unnessecary scripts from package.json

--- serverless/zetta-invoice/webpack.config.js
+++ serverless/zetta-invoice/webpack.config.js
@@ -1,5 +1,5 @@
-var slsw = require('serverless-webpack');
-var nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -9,6 +9,15 @@ module.exports = {
   // Since 'aws-sdk' is not compatible with webpack,
   // we exclude all node dependencies
   externals: [nodeExternals()],
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false,
+  },
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false,
+  },
   // Run babel on all .js files and skip those in node_modules
   module: {
     rules: [
