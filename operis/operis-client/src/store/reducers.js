08c35fe4ed455fee30f5af7a8ed6a875bf01b752
Fredrik Palmquist <fiddep@telia.com>
2018-08-13 19:51:34 +0200

    chore: move files

--- operis/operis-client/src/store/reducers.js
+++ operis/operis-client/src/store/reducers.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import recipient from './Recipients/recipientReducer';
-import invoice from './Invoice/invoiceReducer';
-import customer from './CompanyCustomer/companyCustomerReducer';
+import recipient from '../Recipients/recipientReducer';
+import invoice from '../Invoice/invoiceReducer';
+import customer from '../CompanyCustomer/companyCustomerReducer';
 
 export default combineReducers({
   form: formReducer,
